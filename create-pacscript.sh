4cc497961f255ee77637ea85b14733c4b873cdbe
Henryws <hwengerstickel@gmail.com>
2020-10-27 09:41:53 -0500

    Update create-pacscript.sh

--- create-pacscript.sh
+++ create-pacscript.sh
@@ -3,12 +3,16 @@ printf "name of package? "
 read -r NAME
 printf "command to execute $NAME? "
 read -r PKGNAME
+printf "version (put 1.0 if unsure) "
+read -r VERSION
 printf "url? (must end in zip, tar.xz, or git) "
 read -r URL
 printf "license? "
 read -r LICENSE
 printf "build dependencies? "
 read -r BUILD_DEPS
+printf "dependencies "
+read -r DEPS
 printf "what does $NAME give? (put $NAME if unsure) "
 read -r GIVES
 printf "What does $NAME break? (if any) "
@@ -21,8 +25,28 @@ printf "how do you build? "
 read -r BUILD
 printf "how do you install? (put `sudo porg -lp $NAME "make install"` if unsure) "
 read -r INSTALL
-echo <<EOF
-name=$(echo \"$NAME\")
-pkgname=$(echo \"$PKGNAME\")
-url=$(echo \"$URL\")
-license=$(echo \"$LICENSE\")
+curl https://raw.githubusercontent.com/Henryws/pacstall-tools/master/app.pacscript > /tmp/
+echo <<EOF > $(pwd)/$NAME.pacscript
+name=$NAME
+pkgname=$PKGNAME
+version=$VERSION
+url=$URL
+license=$LICENSE
+build_depends=$BUILD_DEPS
+depends=$DEPS
+gives=$GIVES
+breaks=$BREAKS
+description=$DESC
+
+prepare() {
+$PREPARE
+}
+
+build() {
+$BUILD
+}
+
+install() {
+$INSTALL
+}
+EOF
